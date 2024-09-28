ba6de374dd9fc98e675fcdf4700a3edd77992ccc
Joaquim Angelo <156119400+Joaquim-angelo@users.noreply.github.com>
2024-09-28 14:56:50 -0300

    atualizando...

--- 03-JavaScript/Ex031 (Fetch Api)/ex1.js
+++ 03-JavaScript/Ex031 (Fetch Api)/ex1.js
@@ -1,3 +1,4 @@
+//Fetch serve para solicitar acesso a dados de outros sites.
 //solicitação para o site dummyjson
 fetch('https://dummyjson.com/products')
 //buscando dados dos produtos dummy
--- 03-JavaScript/Ex057(filtro_aplicado)/arquivo.js
+++
@@ -1,6 +0,0 @@
-function selecionar(){
-    const radios=[...document.querySelectorAll("input[type=radio]")]
-    let rad_select=radios.filter((valor)=>{return valor.checked})
-    const curso=rad_select.parentNode.previousSibling.textContent
-    alert(curso)
-}
